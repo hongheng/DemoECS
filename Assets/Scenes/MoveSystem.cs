89c78ab947d3e72949135896866bbb37adfb1be5
HongHeng <hhfire2006@gmail.com>
2018-09-22 16:47:57 +0800

    使用JobSystem

--- Assets/Scenes/MoveSystem.cs
+++ Assets/Scenes/MoveSystem.cs
@@ -1,8 +1,50 @@
-﻿using Unity.Collections;
+﻿#define USE_JOB_SYSTEM
+
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
+
+#if USE_JOB_SYSTEM
+
+public class MoveSystem : JobComponentSystem {
+
+    public struct Data : IJobProcessComponentData<PlayerInput, Position> {
+
+        public float moveSpeed;
+        public float dt;
+
+        public void Execute([ReadOnly]ref PlayerInput input, ref Position position) {
+            var pos = position.Value;
+            var target = input.target;
+            if (math.all(target == pos)) {
+                return;
+            }
+            var diff = target - pos;
+            var lenToGo = math.length(diff);
+            var lenGo = dt * moveSpeed;
+            if (lenGo < lenToGo) {
+                pos += diff * lenGo / lenToGo;
+            } else {
+                pos = target;
+            }
+            position = new Position { Value = pos };
+        }
+    }
+
+    float moveSpeed = 5f;
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps) {
+        return new Data() {
+            dt = Time.deltaTime,
+            moveSpeed = moveSpeed,
+        }.Schedule(this, inputDeps);
+    }
+}
+
+#else
 
 public class MoveSystem : ComponentSystem {
 
@@ -36,3 +78,5 @@ public class MoveSystem : ComponentSystem {
         }
     }
 }
+
+#endif
